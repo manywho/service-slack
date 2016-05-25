5832137d217600f0f1a8722522fd4d66c75544f9
Jonjo McKay <jonjo.mckay@manywho.com>
2016-05-25 14:23:48 +0100

    Made compatible with the latest SDK snapshot

--- src/main/java/com/manywho/services/slack/ApplicationConfiguration.java
+++ src/main/java/com/manywho/services/slack/ApplicationConfiguration.java
@@ -1,10 +1,10 @@
 package com.manywho.services.slack;
 
 import com.manywho.sdk.api.ContentType;
-import com.manywho.sdk.services.configuration.ConfigurationValue;
+import com.manywho.sdk.services.configuration.Configuration;
 
-public class ApplicationConfiguration {
-    @ConfigurationValue(name = "API Token", contentType = ContentType.Password, required = false)
+public class ApplicationConfiguration implements Configuration {
+    @Configuration.Value(name = "API Token", contentType = ContentType.Password, required = false)
     private String apiToken;
 
     public String getApiToken() {
