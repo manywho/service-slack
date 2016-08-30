45d7524955e06aeab3eb02a9b36d1fdfa515c802
Jonjo McKay <jonjo@jonjomckay.com>
2016-08-30 19:55:32 +0100

    Added context path

--- src/main/java/com/manywho/services/slack/Application.java
+++ src/main/java/com/manywho/services/slack/Application.java
@@ -1,24 +1,11 @@
 package com.manywho.services.slack;
 
-import allbegray.slack.SlackClientFactory;
-import allbegray.slack.webapi.SlackWebApiClient;
-import allbegray.slack.type.*;
 import com.manywho.sdk.services.BaseApplication;
-
-import java.util.List;
 
 public class Application extends BaseApplication {
     public static void main(String[] args) throws Exception {
-//        Application application = new Application();
-//        application.startServer();
-
-        SlackWebApiClient webApiClient = SlackClientFactory.createWebApiClient("");
-
-        webApiClient.addReminder("test", 100);
-
-        ReminderList reminderList = webApiClient.getReminderList();
-
-        System.out.println(reminderList);
-
+        Application application = new Application();
+        application.setModule(new ApplicationModule());
+        application.startServer("/api/slack/2");
     }
 }
