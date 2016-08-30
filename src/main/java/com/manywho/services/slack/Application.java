89fa0171afeb276ce3d49e71fa59d0737247d6a2
Jonjo McKay <jonjo@jonjomckay.com>
2016-08-30 20:05:10 +0100

    Removed broken actions

--- src/main/java/com/manywho/services/slack/Application.java
+++ src/main/java/com/manywho/services/slack/Application.java
@@ -1,8 +1,8 @@
 package com.manywho.services.slack;
 
-import com.manywho.sdk.services.BaseApplication;
+import com.manywho.sdk.services.ServiceApplication;
 
-public class Application extends BaseApplication {
+public class Application extends ServiceApplication {
     public static void main(String[] args) throws Exception {
         Application application = new Application();
         application.setModule(new ApplicationModule());
