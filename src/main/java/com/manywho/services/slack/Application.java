46c688ac81f7907215947ad788341fdea4172bfa
Jonjo McKay <jonjo.mckay@manywho.com>
2017-02-13 14:58:24 +0000

    Updated to latest version of SDK 2.0.0-SNAPSHOT

--- src/main/java/com/manywho/services/slack/Application.java
+++ src/main/java/com/manywho/services/slack/Application.java
@@ -1,11 +1,14 @@
 package com.manywho.services.slack;
 
 import com.manywho.sdk.services.ServiceApplication;
+import com.manywho.sdk.services.servers.EmbeddedServer;
+import com.manywho.sdk.services.servers.undertow.UndertowServer;
 
 public class Application extends ServiceApplication {
     public static void main(String[] args) throws Exception {
-        Application application = new Application();
-        application.setModule(new ApplicationModule());
-        application.startServer("/api/slack/2");
+        EmbeddedServer server = new UndertowServer();
+        server.setApplication(Application.class);
+        server.addModule(new ApplicationModule());
+        server.start("/api/slack/2");
     }
 }
--- src/main/java/com/manywho/services/slack/ApplicationConfiguration.java
+++ src/main/java/com/manywho/services/slack/ApplicationConfiguration.java
@@ -4,7 +4,7 @@ import com.manywho.sdk.api.ContentType;
 import com.manywho.sdk.services.configuration.Configuration;
 
 public class ApplicationConfiguration implements Configuration {
-    @Configuration.Value(name = "API Token", contentType = ContentType.Password, required = false)
+    @Configuration.Setting(name = "API Token", contentType = ContentType.Password, required = false)
     private String apiToken;
 
     public String getApiToken() {
